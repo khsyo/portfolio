6ae82a17db0ea155feccbe7a84b85066c5f0a633
hesong <actkhs@gmail.com>
2019-08-13 12:13:11 +0800

    Updates

---
+++ precache-manifest.c8a1e7cfc830e2188d3bc47409ab6d9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b10c3eadcec95b99f0cb93dfaa1c23",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21cbd41e84fdc7bde269",
+    "url": "/portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3ae0ae62234e3825c6e5",
+    "url": "/portfolio/static/css/main.c6288169.chunk.css"
+  },
+  {
+    "revision": "21cbd41e84fdc7bde269",
+    "url": "/portfolio/static/js/2.5e47c77a.chunk.js"
+  },
+  {
+    "revision": "3ae0ae62234e3825c6e5",
+    "url": "/portfolio/static/js/main.d79cc503.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "95a9ee6f6721307d542061c1989249d2",
+    "url": "/portfolio/static/media/canaan_homepage.95a9ee6f.png"
+  },
+  {
+    "revision": "ddfe9ca54ba3402191e6dbc7ceb0c0a2",
+    "url": "/portfolio/static/media/hesong.ddfe9ca5.png"
+  },
+  {
+    "revision": "77e80b58a257bf7998c56f53f4398bcb",
+    "url": "/portfolio/static/media/post_a_job.77e80b58.png"
+  },
+  {
+    "revision": "105c8d34cb23dff3e06ff734ad73ed07",
+    "url": "/portfolio/static/media/sign_up_function.105c8d34.png"
+  }
+]);
