afe260929a7f6c3e41fd1cb0b7fdad845bd40a7e
hesong <actkhs@gmail.com>
2019-09-04 08:19:51 +0800

    Updates

--- precache-manifest.3c5142f9b5899066ca3ba19c2c368cde.js
+++ precache-manifest.3c5142f9b5899066ca3ba19c2c368cde.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e8f7448b0188fe18615783052482441a",
+    "revision": "a758e93fbc8dd0c83e90bedd6374f2fe",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "17fbca8e876a4dd829cc",
-    "url": "/portfolio/static/css/main.40d71449.chunk.css"
+    "revision": "2f24bfbb3c75bf86386b",
+    "url": "/portfolio/static/css/main.5bec0c9d.chunk.css"
   },
   {
-    "revision": "d80a10ccf3ae4f413264",
-    "url": "/portfolio/static/js/2.cca1f315.chunk.js"
+    "revision": "1e487355e63fd92c9b7d",
+    "url": "/portfolio/static/js/2.e48e16d3.chunk.js"
   },
   {
-    "revision": "17fbca8e876a4dd829cc",
-    "url": "/portfolio/static/js/main.0f0a1cc8.chunk.js"
+    "revision": "2f24bfbb3c75bf86386b",
+    "url": "/portfolio/static/js/main.fd0390e6.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a83d686e0a402debcbfa55859bdfcb85.js"
+  "/portfolio/precache-manifest.3c5142f9b5899066ca3ba19c2c368cde.js"
 );
 
 self.addEventListener('message', (event) => {
